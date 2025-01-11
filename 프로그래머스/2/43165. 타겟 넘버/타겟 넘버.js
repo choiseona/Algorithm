2a350e0c75145add8d4e2f3cd874796e71791a8a
choiseona <52223965+choiseona@users.noreply.github.com>
2025-01-11 11:22:49 +0900

    [level 2] Title: 타겟 넘버, Time: 38.39 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+    let answer = 0;
+    
+    const DFS = (level, sum) => {
+        if(level === numbers.length) {
+            if(sum === target) {
+                answer++;
+            }        
+            return;
+        } 
+        DFS(level+1, sum + numbers[level])
+        DFS(level+1, sum - numbers[level])
+    }
+    
+    DFS(0,0)
+    return answer;
+}
