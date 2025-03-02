b8152a7cd48e9bce3e7e373454add8db3e415b73
choiseona <52223965+choiseona@users.noreply.github.com>
2025-03-03 08:32:56 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1492.88 ms, Memory: 384 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
@@ -1,33 +1,35 @@
 function solution(edges) {
-    // 각 노드 별 from 개수와 to 개수의 map 생성
-    const map = new Map();
-    edges.forEach(([from,to]) => {
-        const fromCount = map.get(from) || {from: 0, to: 0};
-        map.set(from, {from: fromCount.from, to: fromCount.to + 1});
-        
-        const toCount = map.get(to) || {from: 0, to: 0};
-        map.set(to, {from: toCount.from + 1, to: toCount.to});        
+    // 도녓 -> n개 정점, n개 간선
+    // 막대 -> n개 정점, n-1개 간선
+    // 8자 -> 2n+1개 젖엄, 2n+2개 간선    
+    // 결과 -> [생성한 정점 번호, 도넛 그래프 수, 막대 그래프 수, 8자 그래프 수]
+    
+    // 생성한 정점 번호: 들어오는 간선 0, 나가는 간선 2개 이상(제한 사항: 그래프는 2개 이상)
+    // 막대: 나가는 간선 0 (생성한 정점에서 간선이 들어오므로 들어오는 간선 0일 때는 제외)
+    // 8자: 들어오는 간선 2 이상, 나가는 간선 2 (들어오는 간선 2 이상인 이유: 생성한 정점에서 들어오는 간선 포함)
+    // 도넛: 생성한 정점의 나가는 간선 개수 - 막대 개수 - 8자 개수 
+    
+    // 그래프 연결하기 
+    const graphs = {};
+    edges.forEach(([from, to]) => {
+        if(!graphs[from]) {
+            graphs[from] = {'from':[],'to':[]}
+        }
+        if(!graphs[to]) {
+            graphs[to] = {'from':[],'to':[]}
+        }
+        graphs[from].to.push(to)
+        graphs[to].from.push(from)
     })
     
-    // 생성한 정점, 그래프 별 개수 구하기
+    // 개수 구하기
     const answer = [0,0,0,0];
-    for(const node of map) {
-        const nodeNumber = node[0];
-        const edgeCount = node[1];
-        
-        // 생성한 정점: from.length = 0 && to.length >= 2
-        if(edgeCount.from === 0 && edgeCount.to >= 2) answer[0] = nodeNumber;
-        
-        // to.length = 0 이면 막대 그래프
-        else if(edgeCount.to === 0) answer[2]++;
-        
-        // to.length = 2이고 from.length >=2이면 8자 모양 그래프
-        else if(edgeCount.to === 2 && edgeCount.from >= 2) answer[3]++;
+    for(const [node, {from, to}] of Object.entries(graphs)) {
+        if(from.length === 0 && to.length >= 2) answer[0] = Number(node);
+        if(to.length === 0) answer[2]++;
+        if(from.length >= 2 && to.length === 2) answer[3]++;
     }
+    answer[1] = graphs[answer[0]].to.length - answer[2] - answer[3];
     
-    // 도넛 그래프: 시작 정점 to.length - 막대 그래프 개수 - 8자 그래프 개수
-    answer[1] = map.get(answer[0]).to - answer[2] - answer[3];
-
-    return answer;
-
+    return answer
 }
