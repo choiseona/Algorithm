54e945990ea99983b32abae5c991cfd07a130769
choiseona <52223965+choiseona@users.noreply.github.com>
2025-03-09 18:17:26 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,43 +1,4 @@
 function solution(n, w, num) {
-    const height = Math.ceil(n/w);
-    const width = w;
-    const boxes = Array.from({length:height}, () => Array.from({length:width}, () => -1));
-    
-    // 박스 채우기
-    // rowIndex가 2로 나눠떨어지면 왼쪽부터, 아니면 오른쪽부터 채우기
-    for(let rowIndex=0; rowIndex<height; rowIndex++) { 
-        if(rowIndex % 2 === 0) {
-            for(let columnIndex=0; columnIndex < width; columnIndex++) {
-                if(rowIndex * w + (columnIndex + 1) > n) continue; 
-                boxes[rowIndex][columnIndex] = rowIndex * w + (columnIndex + 1);
-            }
-        }
-        else {
-            for(let columnIndex=0; columnIndex < width; columnIndex++) {
-                if(rowIndex * w + (width-columnIndex) > n) continue;
-                boxes[rowIndex][columnIndex] = rowIndex * w + (width - columnIndex)
-            }
-        }
-    }
-    
-    // num의 인덱스 index 구해서 y부터 height까지 -1이 아닌 것만 필터링
-    let rowIndexOfNum;
-    let columnIndexOfNum;
-    
-    boxes.forEach((row, rowIndex) => {
-        row.forEach((element, columnIndex) => {
-            if(element === num) {
-                rowIndexOfNum = rowIndex;
-                columnIndexOfNum = columnIndex;
-            }
-        })
-    })
-    
-    let answer = 0;
-    for(let rowIndex=rowIndexOfNum; rowIndex<height; rowIndex++) {
-        if(boxes[rowIndex][columnIndexOfNum] !== -1) answer++;
-    }
-    
-    return answer
-    
+    var answer = 0;
+    return answer;
 }
