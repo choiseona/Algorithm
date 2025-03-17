33dda949938b38ccebf06c8032b5e494c73d8db4
choiseona <52223965+choiseona@users.noreply.github.com>
2025-03-18 04:13:55 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 162.47 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -5,10 +5,12 @@ function solution(land) {
     // 입력: land(석유 덩어리 2차원 정수배열)
     // 출력: 가장 많은 석유 있는 column의 석유량 
     
+    // -------------------------------------
+    
     const height = land.length;
     const width = land[0].length;
 
-    // 1. map 돌면서 2차원 column 배열 갱신 
+    // 하나의 석유 덩어리 좌표들 반환  
     const BFS = (startRow, startCol, visited) => {
         const direction = [[0,-1], [0,1], [-1,0], [1,0]];
         const queue = [];
@@ -32,13 +34,13 @@ function solution(land) {
         return dengAuRies;
     }
     
+    // land 돌면서, BFS로 반환받은 석유 덩어리 정보로 column에 대한 석유량 갱신 
     const visited = Array.from({length:height}, () => Array.from({length:width}), () => false)
     const seokU = Array.from({length:width}, () => 0)
     land.forEach((row, rowIndex) => {
         row.forEach((el, columnIndex) => {
             if(visited[rowIndex][columnIndex] || !land[rowIndex][columnIndex]) return;
             const dengAuRies = BFS(rowIndex, columnIndex, visited)  
-            
             const set = new Set();
             dengAuRies.forEach(([row,col]) => set.add(col));
             set.forEach((col) => seokU[col]+= dengAuRies.length)
