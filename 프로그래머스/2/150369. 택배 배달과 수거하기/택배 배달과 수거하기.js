096c825ad61edaa5960523f9c789f2990f533225
choiseona <52223965+choiseona@users.noreply.github.com>
2025-03-19 16:39:18 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 7.39 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,25 +1,31 @@
 function solution(cap, n, deliveries, pickups) {
-  // cap: 트럭에 실을 수 있는 재활용 택배 상자 최대 개수
-  // n: n개의 집
-  // deliveries: i+1번째 집에 배달할 재활용 택배 상자 개수
-  // pickups: i+1번째 집에서 수거할 빈 재활용 택배 상자의 개수
+    // 2:55 시작 
+    
+    // i번째 집은 물류창고와 i만큼 떨어져있음
+    // i번째 집은 j번째 집과 j-i만큼 떨어져 있음
+    // 재활용 택배 상자에 담아 배달, 빈 재활용 택배 상자 수거
+    // 트럭에 최대 cap개의 재활용 택배 상자 실을 수 있음
+    
+    // 입력: cap(수용 개수), n(집 개수), deliveries(배달 배열), pickups(수거 배열)
+    // 출력: 최소 이동 거리
+    
+    // ------------------------------------------
+    
+    // 가장 먼 곳부터 배달/수거 0보다 클 때까지 왕복   
+    let distance = 0;
+    let deliverCount = 0;
+    let pickupCount=0;
 
-  // 모든 배달과 수거 마치고 물류창고까지 돌아오는 최소 이동 거리 반환
-
-  let answer = 0;
-  let delivery = 0,
-    pickup = 0;
-
-  // delivery-cap 과 pickup-cap의 누적값을 통해 답 구하기
-  for (let i = n - 1; i >= 0; i--) {
-    delivery += deliveries[i];
-    pickup += pickups[i];
-
-    while (delivery > 0 || pickup > 0) {
-      delivery -= cap;
-      pickup -= cap;
-      answer += (i + 1) * 2;
+    for(let home=n-1; home>=0; home--) {  
+        deliverCount += deliveries[home];
+        pickupCount += pickups[home];
+        
+        while(deliverCount > 0 || pickupCount > 0) {
+            deliverCount -= cap;
+            pickupCount -= cap;
+            distance += (home+1)*2
+            
+        }
     }
-  }
-  return answer;
+    return distance;
 }
