977ad5d7bea385fa56eef30a1ca99c1bed8bdf1f
choiseona <52223965+choiseona@users.noreply.github.com>
2025-04-21 09:47:52 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 21.27 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,24 +1,22 @@
 function solution(gems) {
-    // 시작 - 11:16
+    // 시작 - 11:30, 끝 - 12:33
     
     // 모든 종류의 보석을 적어도 1개 이상 포함하는 가장 짧은 구간의 보석 모두 구매
     // 입력: gems(보석들 이름)
     // 출력: [시작 진열대 번호, 끝 진열대 번호]
     
     //-------------------------------------
-    // set, map, 투포인터, 슬라이딩 윈도우 활용
+    // set, map, 투포인터 활용
     
     let allCategoryCount = new Set(gems).size;
-    let left = 0; let right = 0;
-    let minLeft = -1; let minRight = -1;
-    let minLength = Infinity;
+    let left = 0; let right = 0; // 현재 진열대 구간 
+    let minLeft = Infinity; let minRight = Infinity; // 시작 진열대 번호가 가장 작은 구간
     let currentMap = new Map([[gems[0],1]]);
     
     while(right < gems.length) {
         if(allCategoryCount === currentMap.size) {
             const currentLength = right-left+1;
-            if(minLength > right - left + 1) {
-                minLength = currentLength;
+            if(minLeft === Infinity || (minRight - minLeft + 1 > right - left + 1)) {
                 minLeft = left;
                 minRight = right;
             }
