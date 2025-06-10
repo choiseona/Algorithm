07f02310fa730a2d23f43385a5cdf04e38ca09c8
choiseona <52223965+choiseona@users.noreply.github.com>
2025-06-10 09:56:25 +0900

    [level 3] Title: 베스트앨범, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -3,23 +3,15 @@ function solution(genres, plays) {
     const genreTotalPlayCount = {};
     const genreChoiceCount = {};
     
+    // 장르별 play 수 구하기 
+    genres.forEach((genre, index) => genreTotalPlayCount[genre] = (genreTotalPlayCount[genre] || 0) + plays[index] )
+    
     // 노래들 하나의 객체로 나타내기
-    const songArray = genres.map((e, i) => ({genre:e, play: plays[i], index:i}));
+    const songArray = genres.map((e, i) => ({genre:e, play: plays[i], index:i, genreTotal: genreTotalPlayCount[e]}));
     
-    // 장르별 총 play 횟수 구하기
-    songArray.forEach((e) => {
-        const { genre, play } = e;
-        genreTotalPlayCount[genre] = (genreTotalPlayCount[genre] || 0) + play;
-    })
-    
-    //구한 장르별 총 play 횟수 객체에 추가하기
-    const newSongArray = songArray.map((e,i) => {
-        const {genre} = e;
-        return {...e, genreTotal:genreTotalPlayCount[genre]};
-    })
     
     // 장르별 play 수로 정렬 => 개별 play 수로 정렬 
-    const sortedSongArray = newSongArray.sort((a,b) => b.genreTotal - a.genreTotal || b.play - a.play);
+    const sortedSongArray = songArray.sort((a,b) => b.genreTotal - a.genreTotal || b.play - a.play);
    
     // 장르별로 2개 선택
     sortedSongArray.forEach((e) => {
