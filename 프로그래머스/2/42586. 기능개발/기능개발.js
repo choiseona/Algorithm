94f8f7aedf97ca4b2ff1b3cc77a5a3f4d01ef3e7
choiseona <52223965+choiseona@users.noreply.github.com>
2025-06-11 09:58:38 +0900

    [level 2] Title: 기능개발, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,9 +1,10 @@
 function solution(progresses, speeds) {
-    var answer = [0];
+    const answer = [0];
     const remainDays = progresses.map((e, i) =>  Math.ceil((100 - progresses[i]) / speeds[i]));
+    
     let maxRemainDay = remainDays[0];
 
-    for(let i=0, j=0 ; i<remainDays.length; i++){        
+    for(let i=0, j=0; i<remainDays.length; i++){        
         if(remainDays[i] > maxRemainDay) {
             maxRemainDay = remainDays[i];
             answer[++j] = 0;
