acbd1aec4e916caafc2a248adb53b97283557e6f
choiseona <52223965+choiseona@users.noreply.github.com>
2025-06-16 15:42:09 +0900

    [level 2] Title: H-Index, Time: 0.41 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,10 +1,14 @@
 function solution(citations) {
-    let answer = 0;
-    citations.sort((a,b) => b-a);
-    
-    for(let i=0; i<citations.length; i++) {
-        if(citations[i] >= i+1) answer++;
-        
+    // 1. h보다 크거나 같은 수가 h개 이상개여야함 
+    // 2. h보다 작거나 같은 수가 h개 이하여야함 
+    // 0 1 3 5 6 
+
+    citations.sort((a, b) => a - b); 
+
+    for (let i = 0; i < citations.length; i++) {
+        const h = citations.length - i;
+        if (citations[i] >= h) return h;
     }
-    return answer;
+
+    return 0;
 }
