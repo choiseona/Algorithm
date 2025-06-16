95c87af7c01a98a92f0cd261f1fdd8bd5ca4618a
choiseona <52223965+choiseona@users.noreply.github.com>
2025-06-16 15:11:12 +0900

    [level 2] Title: 가장 큰 수, Time: 156.22 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
-    const sortedNumbers = numbers.map((e) => e.toString()).sort((a,b) => (b+a) - (a+b));
-    return (sortedNumbers[0] === '0') ?  "0" : sortedNumbers.join("");
+    const sortedNumbers = numbers.map(String).sort((a,b) => Number(b+a) - Number(a+b))
+    return sortedNumbers[0] === '0' ? '0' : sortedNumbers.join("")
 }
